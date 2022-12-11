1b6dcdee3548dc9393d5ffc5fc0725dbd5b9e4ee
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-10 21:25:02 -0800

    added class prototypes to be implemented later

--- linear_algebra.py
+++ linear_algebra.py
@@ -24,6 +24,56 @@ class Matrix:
         return string
 
 
+    # A + B
+    def __add__(self, other):
+        pass
+
+
+    # A - B
+    def __sub__(self, other):
+        pass
+
+
+    # A * B
+    def __mul__(self, other):
+        pass
+
+
+    # A // B
+    def __floordiv__(self, other):
+        pass
+
+
+    # A / B
+    def __truediv__(self, other):
+        pass
+
+
+    # A**n aka A * A n times
+    def __pow__(self, n):
+        pass
+
+
+    # -A
+    def __neg__(self):
+        pass
+
+
+    # +A
+    def __pos__(self):
+        pass
+
+
+    # | A |
+    def __abs__(self):
+        pass
+
+
+    # A == B
+    def __eq__(self, other):
+        pass
+
+
     # TODO figure out how to handle access out of bounds
     def row(self, n):
         return self.M[n * self.n : n * self.n + self.n]
@@ -32,3 +82,11 @@ class Matrix:
     # TODO figure out how to handle access out of bounds
     def col(self, n):
         return [self.M[self.n * i + n] for i in range(self.m)]
+
+
+    def dot(self, other):
+        pass
+
+
+    def transpose(self):
+        pass
