4c6da39a2ecaab6faed9d436123f58792b5b3d85
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-16 10:00:53 -0700

    added another method to make pi

--- calculus.py
+++ calculus.py
@@ -1,10 +1,16 @@
 # Jacobus Burger (2023)
+# Calculus focues computing maths. Now with PI!
+from math import sqrt, factorial
 
 
 # Srinivasa Ramanujan's pi estimate
-from math import sqrt, factorial
-def sr_pi(n = 0):
+def ramanujan_pi(n = 0):
     sequence = 0
     for i in range(n + 1):
         sequence += (factorial(4*i) * (1103 + (26390 * i))) / (factorial(i)**4 * 396**(4*i))
     return ((2 * sqrt(2))/9801) * sequence
+
+
+# aproximate pi with some triangle magic
+def arc_pi():
+    return atan(1) + atan(2) + atan(3)
