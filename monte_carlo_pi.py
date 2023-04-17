bdb15cf130790b17fac1aef90884d16f125d4289
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-16 18:49:12 -0700

    started cute turtle program

---
+++ monte_carlo_pi.py
@@ -0,0 +1,5 @@
+# Jacobus Burger (2023)
+# Info:
+#   Approximate PI with a monte-carlo simulaton.
+#   Supposing an n by n square around a circle of radius n, the ratio
+#   of random dots in the circle to dots in the square times 4.
