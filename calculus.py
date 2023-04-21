1cae4e72ab4ee6c86bd5e04c563e7fb1e0909c83
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-21 14:50:55 -0700

    encapsulted pi calculation methods

--- calculus.py
+++ calculus.py
@@ -4,34 +4,36 @@ from math import sqrt, factorial, atan
 from random import uniform
 
 
-# Srinivasa Ramanujan's pi estimate
-def ramanujan_pi(n = 0):
-    sequence = 0
-    for i in range(n + 1):
-        sequence += (factorial(4*i) * (1103 + (26390 * i))) / (factorial(i)**4 * 396**(4*i))
-    return (((2 * sqrt(2))/9801) * sequence) ** -1  # 1/pi -> pi
+# various ways to calculate pi
+class PI:
+    # Srinivasa Ramanujan's pi estimate
+    def ramanujan_pi(n = 0):
+        sequence = 0
+        for i in range(n + 1):
+            sequence += (factorial(4*i) * (1103 + (26390 * i))) / (factorial(i)**4 * 396**(4*i))
+        return (((2 * sqrt(2))/9801) * sequence) ** -1  # 1/pi -> pi
 
 
-# aproximate pi with some triangle magic
-def arc_pi():
-    return atan(1) + atan(2) + atan(3)
+    # aproximate pi with some triangle magic
+    def arc_pi():
+        return atan(1) + atan(2) + atan(3)
 
 
-# sometimes pi can be approximated with a rational (fraction)
-def fractional_pi():
-    return 22/7
+    # sometimes pi can be approximated with a rational (fraction)
+    def fractional_pi():
+        return 22/7
 
 
-# calculate pi with John Machin formula (circ. 1706)
-def machin_pi():
-    return 4 * (4 * atan(1/5) + atan(1/239))
+    # calculate pi with John Machin formula (circ. 1706)
+    def machin_pi():
+        return 4 * (4 * atan(1/5) + atan(1/239))
 
 
-# calculate pi with Monte-Carlo method
-def monte_carlo_pi(n = 1000):
-    circle, square = 0, 0
-    for _ in range(n):
-        if sqrt(uniform(-1, 1)**2 + uniform(-1, 1)**2) <= 1:
-            circle += 1
-        square += 1
-    return (circle / square) * 4
+    # calculate pi with Monte-Carlo method
+    def monte_carlo_pi(n = 1000):
+        circle, square = 0, 0
+        for _ in range(n):
+            if sqrt(uniform(-1, 1)**2 + uniform(-1, 1)**2) <= 1:
+                circle += 1
+            square += 1
+        return (circle / square) * 4
