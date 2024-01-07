1858ab5784d06a3b6faa2f80b854eb0dafe206c3
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-07 00:03:59 -0800

    bugfix: used sorting instead of monotonic queues (wrong application here)

--- kaprekar.py
+++ kaprekar.py
@@ -16,21 +16,10 @@ def count_steps(n, delay=0):
     """
     total_steps = 0
     while n != KAPREKAR_CONSTANT:
-        # get ascending and descending arrangements using monotonic queues
-        #   in linear time
-        # BUG: 8082 becomes 2088 and 8802 instead of 0288 and 8820
-        current = str(n)
-        increasing = deque()
-        decreasing = deque()
-        for digit in current:
-            print(digit, increasing, decreasing)
-            if len(increasing) > 0 and int(digit) >= int(increasing[-1]):
-                increasing.append(digit)
-                decreasing.appendleft(digit)
-            else:
-                decreasing.append(digit)
-                increasing.appendleft(digit)
-        print(n, current, increasing, decreasing)
+        # sort into increasing and decreasing numeric arrangement
+        digits = [*str(n)]
+        increasing = sorted(digits)
+        decreasing = sorted(digits, reverse=True)
         # calculate next number in sequence
         n = int(''.join(decreasing)) - int(''.join(increasing))
         # count step
