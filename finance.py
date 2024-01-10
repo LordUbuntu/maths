0837ff313cb0e4385a3f322c3d7533ebd26187a7
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-09 18:08:29 -0800

    added amortization

--- finance.py
+++ finance.py
@@ -10,6 +10,17 @@ def compound_interest(P, r, n, t):
     return P * (1 + r/n)**(n*t)
 
 
+def amortization(P, r, n, t):
+    """
+    calculate amortization over a period of time.
+        P = principal
+        r = interest rate
+        t = number of periods
+        n = payments per period
+    """
+    return (P * (r/n)) / (1 - (1 + r/n)**(-n*t))
+
+
 # total return from mutual funds
 def total_return(units,
         cost_per_unit,
@@ -26,6 +37,5 @@ def total_return(units,
 # unrealized capital loss/gain over period
 def unrealized_capital_loss_or_gain(units, beginning_navps, ending_navps):
     return (units * beginning_navps) - (units * ending_navps)
-
 
 
