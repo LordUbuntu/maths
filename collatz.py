1d096a366871bf8cb2e4a5e70df07e0905298edd
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-11 20:09:22 -0800

    added function to get direct results

--- collatz.py
+++ collatz.py
@@ -9,7 +9,7 @@ def flat(generator):
     return [*generator]
 
 
-def collatz(n):
+def collatz_generator(n):
     """
     The collatz function creates a generator for the collatz conjecture
     starting at n and ending when n == 1.
@@ -27,6 +27,19 @@ def collatz(n):
         else:
             n = n * 3 + 1
         step += 1
+
+
+def collatz(n):
+    step = 0
+    while n != 1:
+        # if n is even, divide n by 2
+        if n % 2 == 0:
+            n = n // 2
+        # if n is odd, multiply n by 3 then add 1
+        else:
+            n = n * 3 + 1
+        step += 1
+    return step
 
 
 # one obvious pattern to observe: when we hit _any_ power of 2, the sequence becomes linear and will complete from that point in log2 of n steps. How to predict the point where the collatz conjecture will land on this path thought, that doesn't seem so obvious.
