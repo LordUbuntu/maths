3ebe0a6784f1b61d01ec57c71e6cb2ca43abff63
Jacobus Burger <therealjacoburger@gmail.com>
2024-03-16 19:57:07 -0700

    note

--- accounting.py
+++ accounting.py
@@ -5,7 +5,7 @@ from collections import deque
 # TODO: More to add
 
 # Assets = Liabilities + Owner Equity + Revenue - Expenses - Owner Draws
-# Income, Expenses, Fixed and Variable Cost, Total Equity, Inventory
+# Income, Expenses, Fixed and Variable Cost, Total Equity, Inventory, Cost of Goods Sold
 
 def operating_cycle(days_sales_in_inventory, collection_priod):
     return days_sales_in_inventory + collection_period
