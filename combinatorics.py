82ca0d1af16ad5bf3c23c20eeb0c5ba61828ecc5
Jacobus Burger <therealjacoburger@gmail.com>
2024-06-12 14:44:19 -0700

    added factorial function

--- combinatorics.py
+++ combinatorics.py
@@ -1,6 +1,7 @@
 # Created by Jacobus Burger (2024)
 # Info:
 #   This file contains functions and constants for combinatorics
+from functools import cache
 
 
 # Combinations
@@ -11,3 +12,11 @@ def C(n, k: int) -> int:
 # Permutations
 def P(n, k):
     return fact(n) // fact(n - k)
+
+
+# factorial function
+@cache
+def factorial(n: int) -> int:
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
