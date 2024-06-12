9bfdc9715fb4a6fe9f2f61deacf50a823cbdd6cb
Jacobus Burger <therealjacoburger@gmail.com>
2024-06-12 14:35:51 -0700

    added combinations

--- combinatorics.py
+++ combinatorics.py
@@ -1,3 +1,8 @@
 # Created by Jacobus Burger (2024)
 # Info:
 #   This file contains functions and constants for combinatorics
+
+
+# Combinations
+def C(n, k: int) -> int:
+    return fact(n) // (fact(k) * fact(n - k))
