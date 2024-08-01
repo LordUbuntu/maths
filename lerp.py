e45e5df01e7c0d164e1276dbb5faa996eea7603d
Jacobus Burger <therealjacoburger@gmail.com>
2024-08-01 13:44:10 -0700

    created lerp file

---
+++ lerp.py
@@ -0,0 +1,20 @@
+# Jacobus Burger (2024-08-01)
+# Desc:
+#   I was trying to solve a problem with smoothing RPM data from my
+#   tachometry code (tacho.py) while working on the Robotics Research
+#   Project at TWU. I talked about the problem with Finian Lugtigheid
+#   to see if there was a better way to do it than the average over a
+#   dequeue. 
+#   He proposed LERPing, and we figured out a way to adjust the alpha
+#   value dynamically for situations where the wheels stops or changes
+#   direction. This was perfect and I realized that it has many more
+#   applications as well! Thanks Finian!
+# Info:
+#   https://en.wikipedia.org/wiki/Linear_interpolation
+#   https://rachsmith.com/lerp/
+# What:
+#   Linear intERPolation of a polynomial function.
+# Uses:
+#   - Smoothing datapoints
+#   - Computer Graphics
+
