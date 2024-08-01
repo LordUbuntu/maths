a7b0ed032ed327ba33a3963e153d5428da815d6b
Jacobus Burger <therealjacoburger@gmail.com>
2024-08-01 14:36:59 -0700

    added clamp.py

---
+++ clamp.py
@@ -0,0 +1,10 @@
+# Jacobus Burger (2024-08-01)
+# Desc:
+#   Clamping is a useful mathematical function when you want to ensure a
+#   value remains fixed within a bound.
+# Info:
+#   https://en.wikipedia.org/wiki/Clamping_(graphics)
+# What:
+#   Limits values within a specified minimum and maximum value
+# Uses:
+#   - keeping values within an interval (inclusive)
