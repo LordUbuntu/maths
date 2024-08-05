1ebed4bcee0cf27cd8285a7576053bda876deb82
Jacobus Burger <therealjacoburger@gmail.com>
2024-08-04 18:28:06 -0700

    🧪 simplified property testing

--- clamp.py
+++ clamp.py
@@ -8,32 +8,32 @@
 #   Limits values within a specified minimum and maximum value
 # Uses:
 #   - keeping values within an interval (inclusive)
+import typing
+
 def clamp(value: int | float, minimum: int | float, maximum: int | float) -> int | float:
     return max(minimum, min(value, maximum))
 
 
-# Property Based Testing:
-# 1. middle: if min >= max, value == min
-# 2. id: if all values are the same, they should return the value of value
-# 3. min: if value is <= min, value == min
-# 4. max: if value is >= max, value == max
-# 5. interval: value must be in interval [min, max]
 
+
+
+# Properties of clamp function:
+# 0. clamp(a, b, c) == clamp(a, b, c)
+#       for the same input, clamp must provide the same output
+# 1. if min >= max, out == min
+#       output is always at least lower bound
+# 2. if min < max, min <= out <= max
+#       output is bounded in interval [min, max]
 from hypothesis import given
-from hypothesis.strategies import floats, integers
+from hypothesis.strategies import one_of, floats, integers
+import decimal
+import fractions
 
-@given(integers(), integers(), integers())
-def test_clamp_int(value: int, minimum: int, maximum: int):
+
+def test_clamp(value: int | float, minimum: int | float, maximum: int | float) -> None:
     result = clamp(value, minimum, maximum)
+    assert result == clamp(value, minimum, maximum)
     if minimum >= maximum:
         assert result == minimum
-    elif minimum == maximum:
-        assert result == minimum
-        assert result == maximum
-    elif value <= minimum:
-        assert result == minimum
-    elif value >= maximum:
-        assert result == maximum
-    else:
-        assert minimum <= result
-        assert result <= maximum
+    elif minimum < maximum:
+        assert minimum <= result <= maximum
