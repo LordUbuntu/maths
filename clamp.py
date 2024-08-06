1020ca4299298a48c33bc497859467f5a5f5a769
Jacobus Burger <therealjacoburger@gmail.com>
2024-08-05 19:28:38 -0700

    🧪 successful property tests on integers AND floats (including undefined inputs)!!!

--- clamp.py
+++ clamp.py
@@ -8,40 +8,49 @@
 #   Limits values within a specified minimum and maximum value
 # Uses:
 #   - keeping values within an interval (inclusive)
-from math import isfinite
-import decimal
+from math import isfinite, nan, isnan
 from hypothesis import given
 from hypothesis.strategies import one_of, none, integers, floats, fractions, decimals
 
 
-
 def clamp(value: int | float, minimum: int | float, maximum: int | float) -> int | float:
+    # return nan for undefined inputs
     if value is None or minimum is None or maximum is None:
-        return float('nan')
+        return nan
+    if not isfinite(value) or not isfinite(minimum) or not isfinite(maximum):
+        return nan
+    # return clamp of function otherwise
     return max(minimum, min(value, maximum))
 
 
-
-
-
 # Properties of clamp function:
-# 0. clamp(a, b, c) == clamp(a, b, c)
+# 0. if value, min, max == None, NaN, Infinity, etc, return NaN
+#       undefined inputs return an undefined output (nan)
+# 1. clamp(a, b, c) == clamp(a, b, c)
 #       for the same input, clamp must provide the same output
-# 1. if min >= max, out == min
+# 2. if min >= max, out == min
 #       output is always at least lower bound
-# 2. if min < max, min <= out <= max
+# 3. if min < max, min <= out <= max
 #       output is bounded in interval [min, max]
-# 3. if value, min, max == None, NaN, Infinity, etc, return NaN
-#       undefined inputs return an undefined output
 @given(
-    value=one_of(none(), decimals(), floats(), fractions(), integers()),
-    minimum=one_of(none(), decimals(), floats(), fractions(), integers()),
-    maximum=one_of(none(), decimals(), floats(), fractions(), integers()),
+    value=one_of(none(), floats(), fractions(), integers()),
+    minimum=one_of(none(), floats(), fractions(), integers()),
+    maximum=one_of(none(), floats(), fractions(), integers()),
 )
 def test_clamp(value: int | float, minimum: int | float, maximum: int | float) -> None:
+    # 0
+    if value is None or minimum is None or maximum is None:
+        assert isnan(clamp(value, minimum, maximum))
+        return
+    if not isfinite(value) or not isfinite(minimum) or not isfinite(maximum):
+        assert isnan(clamp(value, minimum, maximum))
+        return
+    # 1
     result = clamp(value, minimum, maximum)
     assert result == clamp(value, minimum, maximum)
+    # 2
     if minimum >= maximum:
         assert result == minimum
+    # 3
     elif minimum < maximum:
         assert minimum <= result <= maximum
