ab3c5db9bda08c5c095cacbd81b27afd430aebf1
Jacobus Burger <therealjacoburger@gmail.com>
2024-08-14 16:49:09 -0700

    🧪 removed None tests

--- hamming_distance.py
+++ hamming_distance.py
@@ -27,49 +27,32 @@ from hypothesis.strategies import one_of, none, lists, integers, floats, fractio
 # find the hamming distance of any two strings
 # this should work with lists, strings, and other sequential data types
 def hamming_distance(a: T, b: T) -> int:
-    # satisfy preconditions
-    if a is None and b is None:
-        return 0
-    if a is None and b is not None:
-        return len(b)
-    if a is not None and b is None:
-        return len(a)
     # haming distance + difference of lengths
     return sum(map(op.ne, a, b)) + abs(len(a) - len(b))
 
 
 
 # properties
-# 0. if either undefined, return other's length
-# 1. if both undefined, return 0
-# 2. identity: if a == b, hamming_distance(a, b) == 0
-# 3. idempotence: doing the same operation twice should yield the same output
-# 4. mismatching lengths truncate longer list and count extra length to hamming distance
+# 1. identity: if a == b, hamming_distance(a, b) == 0
+# 2. idempotence: doing the same operation twice should yield the same output
+# 3. mismatching lengths truncate longer list and count extra length to hamming distance
 # TODO:
 # - read whole series to understand property based testing better
 # - https://fsharpforfunandprofit.com/posts/property-based-testing-2/
 @given(
-    a=one_of(none(), lists(integers()), lists(floats())),
-    b=one_of(none(), lists(integers()), lists(floats())),
+    a=one_of(lists(integers()), lists(floats())),
+    b=one_of(lists(integers()), lists(floats())),
 )
 def test_hamming_distance(a: T, b: T):
-    # 0. if either undefined, return other's length
-    if a is None and b is not None:
-        return len(b)
-    if a is not None and b is None:
-        return len(a)
-    # 1. if both undefined, return 0
-    if a is None and b is None:
-        return 0
-    # 1. identity
+    # 1. identity: if a == b, hamming_distance(a, b) == 0
     if a == b:
         assert hamming_distance(a, b) == 0
-    # 2. idempotence
+    # 2. idempotence: doing the same operation twice should yield the same output
     assert hamming_distance(a, a) == hamming_distance(a, a)
     assert hamming_distance(b, b) == hamming_distance(b, b)
     assert hamming_distance(a, b) == hamming_distance(a, b)
     assert hamming_distance(b, a) == hamming_distance(b, a)
-    # 3. mismatch length should return total of hamming and length diff
+    # 3. mismatching lengths truncate longer list and count extra length to hamming distance
     if len(a) != len(b):
         if len(a) > len(b):
             length_diff = len(a) - len(b)
