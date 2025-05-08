e64d5242d9be1df1eb1f1849839edf566b1af917
Jacobus Burger <therealjacoburger@gmail.com>
2025-05-07 23:57:50 -0700

    test: associativity and commutativity are not properties of hamming distance to my knowledge

--- tests/test_hamming_distance.py
+++ tests/test_hamming_distance.py
@@ -24,32 +24,6 @@ ST_strings = st.one_of(
 # TODO: implement oracle to test behaviour itself
 
 
-@given(
-    a=ST_strings,
-    b=ST_strings,
-    c=ST_strings,
-)
-def test_associative_binary_operation_hamming_distance(
-    a: T_strings, b: T_strings, c: T_strings
-) -> None:
-    left = hamming_distance.hamming_distance(
-        a=a, b=hamming_distance.hamming_distance(a=b, b=c)
-    )
-    right = hamming_distance.hamming_distance(
-        a=hamming_distance.hamming_distance(a=a, b=b), b=c
-    )
-    assert left == right, (left, right)
-
-
-@given(a=ST_strings, b=ST_strings)
-def test_commutative_binary_operation_hamming_distance(
-    a: T_strings, b: T_strings
-) -> None:
-    left = hamming_distance.hamming_distance(a=a, b=b)
-    right = hamming_distance.hamming_distance(a=b, b=a)
-    assert left == right, (left, right)
-
-
 @given(a=ST_strings)
 def test_identity_binary_operation_hamming_distance(a: T_strings) -> None:
     identity = []
