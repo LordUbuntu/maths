8325d118964f27d45ccf8370d7c9815e2d6e9872
Jacobus Burger <therealjacoburger@gmail.com>
2025-05-30 20:50:33 -0700

    reformat: collatz.py

--- maths/collatz.py
+++ maths/collatz.py
@@ -27,6 +27,7 @@ def collatz_generator(n: int) -> Generator[tuple[int, int]]:
 
 
 def collatz(n: int) -> int:
+    """Calculate steps for collatz conjecture."""
     step = 0
     while n != 1:
         n = n // 2 if n % 2 == 0 else n * 3 + 1
@@ -34,4 +35,7 @@ def collatz(n: int) -> int:
     return step
 
 
-# one obvious pattern to observe: when we hit _any_ power of 2, the sequence becomes linear and will complete from that point in log2 of n steps. How to predict the point where the collatz conjecture will land on this path thought, that doesn't seem so obvious.
+# One obvious pattern to observe: when we hit _any_ power of 2, the sequence
+#   becomes linear and will complete from that point in log2 of n steps. How
+#   to predict the point where the collatz conjecture will land on this path
+#   thought, that doesn't seem so obvious.
