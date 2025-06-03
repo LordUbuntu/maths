fa99fb69ea0fb9f8fb10621704854783f8efdd9e
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-02 20:17:49 -0700

    tests: begin test_collatz.py

---
+++ tests/test_collatz.py
@@ -0,0 +1,6 @@
+# Link:
+# - University of Notre Dame:
+#   https://www3.nd.edu/~pbui/teaching/cse.30872.fa22/challenge05.html
+#
+# This will be challenging. Rough range of O is O(n^3)
+from hypothesis import given, strategies as st
