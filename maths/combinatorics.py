59c3a917dfe8ad01268be647984a360214387101
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-02 17:40:30 -0700

    refactor: reformat combinatorics.py to meet formatting standards

--- maths/combinatorics.py
+++ maths/combinatorics.py
@@ -1,12 +1,14 @@
 # Created by Jacobus Burger (2024)
 # Info:
 #   This file contains functions and constants for combinatorics
+from collections.abc import Generator
 from functools import cache
 
 
 # factorial function
 @cache
 def factorial(n: int) -> int:
+    """Calculate factorial of n."""
     if n <= 1:
         return 1
     return n * factorial(n - 1)
@@ -14,13 +16,15 @@ def factorial(n: int) -> int:
 
 # Permutation
 # https://en.wikipedia.org/wiki/Permutation
-def P(n, k):
+def permutation(n: int, k: int) -> int:
+    """Calculate permutations P(n, k)."""
     return factorial(n) // factorial(n - k)
 
 
 # Combination / Binomial Coefficient
 # https://en.wikipedia.org/wiki/Combination
-def C(n, k: int) -> int:
+def combination(n: int, k: int) -> int:
+    """Calculate combinations C(n, k)."""
     return factorial(n) // (factorial(k) * factorial(n - k))
     # or: return P(n, k) // P(k, k)
 
@@ -28,7 +32,8 @@ def C(n, k: int) -> int:
 # Combinatorics on Words
 # Prouhet-Thue-Morse sequence (parity sequence)
 # https://en.wikipedia.org/wiki/Thue%E2%80%93Morse_sequence
-def PTM(length):
+def ptm(length: int) -> Generator[int]:
+    """Calculate Prouget-Thue-Morse parity sequence."""
     bit = True
     for n in range(length):
         if ((n ^ (n - 1)).bit_length() + 1) & 1 == 0:
