39b61fd0f358dbd218c89c7cb9a1e8c1115cf595
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-06 21:37:30 -0700

    be explicit with modern type annotation

--- tests/test_clamp.py
+++ tests/test_clamp.py
@@ -7,7 +7,6 @@ from hypothesis import given, strategies as st
 st_int_float = st.one_of(
     st.floats(allow_infinity=False, allow_nan=False), st.integers()
 )
-T_int_float = int | float
 
 
 @given(
@@ -16,6 +15,6 @@ T_int_float = int | float
     maximum=st_int_float,
 )
 def test_fuzz_clamp(
-    value: T_int_float, minimum: T_int_float, maximum: T_int_float
+    value: int | float, minimum: int | float, maximum: int | float
 ) -> None:
     clamp.clamp(value=value, minimum=minimum, maximum=maximum)
