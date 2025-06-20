c6ab11e33a20b77905173caddf31426ada0b9743
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-20 14:09:13 -0700

    changed to use lists

--- maths/distance.py
+++ maths/distance.py
@@ -60,21 +60,21 @@ def euclidean(
     return sqrt(sum((p - q) ** 2 for p, q in zip(ps, qs)))
 
 
-@deal.pre(lambda ps, qs: (ps is not None and ps != ()) and (qs is not None and qs != ()))
+@deal.pre(lambda ps, qs: (ps is not None and ps != []) and (qs is not None and qs != []))
 @deal.pre(lambda ps, qs: all(isfinite(p) for p in ps) and all(isfinite(q) for q in qs))
 @deal.pure
 def manhattan(
-    ps: tuple[int | float], qs: tuple[int | float]
+    ps: list[int | float], qs: list[int | float]
 ) -> int | float:
     """
     Calculate the manhattan distance between two points p and q.
 
     Parameters
     ----------
-    ps: tuple[int | float]
-        Coordinate point of P represented as an n-tuple.
-    qs: tuple[int | float]
-        Coordinate point of Q represented as an n-tuple.
+    ps: list[int | float]
+        Coordinate point of P.
+    qs: list[int | float]
+        Coordinate point of Q.
 
     Returns
     -------
@@ -83,7 +83,7 @@ def manhattan(
 
     Preconditions
     -------------
-    Inputs cannot be None, +/-Inf, Nan, or empty tuples.
+    Inputs cannot be None, +/-Inf, Nan, or [].
     Both P and Q must be in the same dimensionaltiy or missing dimensions
         will be assumed to have a value of 0.
 
@@ -92,9 +92,9 @@ def manhattan(
     """
     # correct missing dimensions
     if len(ps) < len(qs):
-        ps = ps + tuple(0 for _ in range(len(qs) - len(ps)))
+        ps.extend(0 for _ in range(len(qs) - len(ps)))
     if len(ps) > len(qs):
-        qs = qs + tuple(0 for _ in range(len(ps) - len(qs)))
+        qs.extend(0 for _ in range(len(ps) - len(qs)))
     # for 1D (note: same as euclidean)
     if len(ps) == 1 and len(qs) == 1:
         return abs(ps[0] - qs[0])
