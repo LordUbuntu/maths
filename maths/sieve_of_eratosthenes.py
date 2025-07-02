b47dbe0e7672fb1965be415e4a0b680d43c6809b
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-01 20:47:00 -0700

    add sieve from LordUbuntu/algorithms

---
+++ maths/sieve_of_eratosthenes.py
@@ -0,0 +1,17 @@
+# Jacobus Burger (2022)
+# Sieve of Eratosthenes in Python
+# see: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+from math import sqrt as sqrt
+
+
+def sieve(n: int):
+    # initialize all except 0 and 1 to True
+    primes = [True if i > 1 else False for i in range(n + 1)] # iterate through all integers from 2 to sqrt(n)
+    for p in range(2, int(sqrt(n)) + 1):
+        # skip any non-primes
+        if primes[p] == True:
+            # eliminate all multiples i of current prime p, from p^2 to n
+            for i in range(p**2, n + 1, p):
+                primes[i] = False
+    # you now have the sieve of eratosthenes
+    return primes
