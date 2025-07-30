490b582bc1b18900df8fa0fb6b5464e99a4aca0e
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-29 17:30:34 -0700

    improvement tweaks for sieve

--- maths/sieve_of_eratosthenes.py
+++ maths/sieve_of_eratosthenes.py
@@ -4,12 +4,13 @@
 from math import sqrt as sqrt
 
 
-def sieve(n: int):
+def sieve(n: int) -> list[bool]:
     # initialize all except 0 and 1 to True
-    primes = [True if i > 1 else False for i in range(n + 1)] # iterate through all integers from 2 to sqrt(n)
+    primes = [True if i > 1 else False for i in range(n + 1)]
+    # iterate through all integers from 2 to sqrt(n)
     for p in range(2, int(sqrt(n)) + 1):
         # skip any non-primes
-        if primes[p] == True:
+        if primes[p]:
             # eliminate all multiples i of current prime p, from p^2 to n
             for i in range(p**2, n + 1, p):
                 primes[i] = False
